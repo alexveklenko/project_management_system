1b1a1cae724b130ef315e0b78ad4bc8215703ee2
alexbush <alexi.veklenko@gmail.com>
2019-06-11 18:50:26 +0300

    added single task

--- tasks/views.py
+++ tasks/views.py
@@ -6,3 +6,8 @@ from .models import Task
 def index(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/index.html', {'tasks': tasks})
+
+
+def task_view(request, id):
+    task = Task.objects.get(id=id)
+    return render(request, 'tasks/single_task.html', {'task': task})
